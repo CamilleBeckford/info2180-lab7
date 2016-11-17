716de95c2913175c90613b73e33ba99d346702df
Camille Beckford <camille.beckford04@mymona.uwi.edu>
2016-11-17 19:25:20 +0000

    CB

--- world.php
+++ world.php
@@ -9,7 +9,7 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 $country = $_GET['country'];
-echo $country;
+
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -18,6 +18,7 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $result = $conn->query($sql);
 
 echo '<ul>';
+
 foreach ($results as $row) {
   echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
 }
